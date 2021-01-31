6ee96a1d458d2fe92f7871f84dc6a084eb3c0eb2
Alexander Nikolaev <nikolaev.warrior@gmail.com>
2021-01-31 22:46:28 +0500

    Setup connections

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coloredView: UIView!
+    @IBOutlet weak var colorOfSliderOne: UILabel!
+    @IBOutlet weak var colorOfSliderTwo: UILabel!
+    @IBOutlet weak var colorOfSliderThree: UILabel!
+    @IBOutlet weak var valueOfSliderOne: UILabel!
+    @IBOutlet weak var valueOfSliderTwo: UILabel!
+    @IBOutlet weak var valueOfSliderThree: UILabel!
+    @IBOutlet weak var sliderOne: UISlider!
+    @IBOutlet weak var sliderTwo: UISlider!
+    @IBOutlet weak var sliderThree: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func sliderOneMoving() {
+    }
+    @IBAction func sliderTwoMoving() {
+    }
+    @IBAction func sliderThreeMoving() {
+    }
+    
 
 }
 
